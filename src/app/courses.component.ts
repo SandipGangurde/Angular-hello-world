6000bd553509f22fc105aaa54a260425f484c498
Sandip Patil <sandip50sandip@gmail.com>
2021-11-22 18:30:51 +0530

    Section 3 start: Property Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,14 +4,21 @@ import { CoursesService } from "./courses.service";
     selector: 'courses',
     // template: '<div>{{ title }}</div>'
     // template: '<div>{{ getTitle() }}</div>'
-    template: `
-            <h2> {{ title }} </h2>
-            <ul>
-                <li *ngFor="let course of courses">
-                    {{course}}
-                </li>
-            </ul>
-        `
+    // template: `
+    //         <h2> {{ title }} </h2>
+    //         <ul>
+    //             <li *ngFor="let course of courses">
+    //                 {{course}}
+    //             </li>
+    //         </ul>
+    //     `
+    template:`
+        <h2> {{ title }} </h2>
+        <h2 [textContent]="title"></h2>
+        
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" />
+    `
   })
   export class CoursesComponent {
       title="List of courses";
@@ -21,7 +28,7 @@ import { CoursesService } from "./courses.service";
     //   }
 
         courses;
-
+        imageUrl="https://images.indianexpress.com/2020/04/online759.jpg";
         constructor(service: CoursesService) {
             //let service = new CoursesService();
             this.courses = service.getCourses();
