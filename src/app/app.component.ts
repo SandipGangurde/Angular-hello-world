b62da89fdb42d272463dd883251a86a28b965d3d
Sandip Patil <sandip50sandip@gmail.com>
2021-11-24 10:03:36 +0530

    Output Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,7 @@ export class AppComponent {
     title:"Title",
     isFavorite:true
   }
+  onFavoriteChanged(){
+    console.log("Favorite Changed");
+  }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'; //'1st way of the input parameter
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'; //'1st way of the input parameter
 //import { Component, OnInit } from '@angular/core'; //2nd way of the input parameter
 @Component({
   selector: 'app-favorite',
@@ -7,16 +7,20 @@ import { Component, Input, OnInit } from '@angular/core'; //'1st way of the inpu
   //inputs: ['isFavorite'] //2nd way of the input parameter
 })
 export class FavoriteComponent implements OnInit {
+  title:any;
+  @Input('isFavorite') isSelected: boolean = false; //'1st way of the input parameter
+
+  @Output() change = new EventEmitter();
 
   constructor() { }
   
   ngOnInit(): void {
   }
-  title:any;
-  @Input('isFavorite') isSelected: boolean = false; //'1st way of the input parameter
+  
   //isFavorite: boolean = false;
   onClick() {
     this.isSelected = !this.isSelected;
+    this.change.emit();
   }
 
 }
