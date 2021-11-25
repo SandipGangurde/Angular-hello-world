c82f6c53532602f6a43e462b380a56444faa3fcc
Sandip Patil <sandip50sandip@gmail.com>
2021-11-25 15:29:29 +0530

    Working with Drop-Down Lists

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Form'},
+    {id: 3, name: 'Comment'}
+  ];
   log(x:any) { 
     console.log(x); 
   }
