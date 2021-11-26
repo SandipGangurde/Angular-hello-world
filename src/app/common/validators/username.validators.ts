00f5d32b653355b2ed556b2ebb0b6e3f48c89feb
Sandip Patil <sandip50sandip@gmail.com>
2021-11-26 12:55:42 +0530

    96. Asynchronous Operations : problem which is resolve by next unit. bcoz alway return null value.

--- src/app/common/validators/username.validators.ts
+++ src/app/common/validators/username.validators.ts
@@ -4,7 +4,18 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export class UsernameValidators {
     static cannotContainSpace( control:AbstractControl ) : ValidationErrors | null {
         if((control.value as string).indexOf(' ') >= 0) 
-            return {"cannotContainSpace": true}
+            return {"cannotContainSpace": true};
+
+        return null;
+    }
+
+    static shouldBeUnique(control : AbstractControl) :  ValidationErrors | null {
+        setTimeout(() => {
+            console.log('ok');
+            if(control.value==='sandip')
+                return { "shouldBeUnique": true };
+            return null;
+        }, 2000);
 
         return null;
     }
