2f0e934fa50bcc7a3fcbf69336bdb3db1b3da049
Sandip Patil <sandip50sandip@gmail.com>
2021-11-30 17:03:36 +0530

    HttpServices this unit is so many concepts is diprecated.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,7 @@
+import { NotFoundError } from './../common/validators/not-found-error';
 import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
+import { AppError } from '../common/validators/app-error';
 
 @Component({
   selector: 'app-posts',
@@ -32,7 +34,15 @@ export class PostsComponent implements OnInit {
         .subscribe(response => {
           this.posts.splice(0, 0, post);
           //  console.log(response);
-      });
+      },
+      (error: Response) => {
+      if(error.status === 400){
+        //this.form.setErrors(error.json())
+      }else{
+        alert('An unexpected error occurred.');
+        console.log(error);
+      }
+    });
   }
   updatePost(post: HTMLInputElement){
     this.service.updatePost(post)
@@ -43,9 +53,18 @@ export class PostsComponent implements OnInit {
 
   deletePost(post: HTMLInputElement) {
     this.service.deletePost(post.id)
-    .subscribe(response => {
+    .subscribe(
+      response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
+    },
+    (error: AppError) => {
+      if(error instanceof NotFoundError){
+        alert('This post has already been deleted.');
+      }else{
+        alert('An unexpected error occurred.');
+        console.log(error);
+      }
     });
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AppError } from '../common/validators/app-error';
+import { NotFoundError } from '../common/validators/not-found-error';
+
+import { catchError } from 'rxjs/operators';
+import 'rxjs/add/operator/catch'
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +28,20 @@ export class PostService {
   }
 
   deletePost(id:any){
-    return this.http.delete(this.url + '/' + id);
+    return this.http.delete(this.url + '/' + id)
+    // .catch((error: Response) => {
+    //   Observable.throw new Error("");
+      
+    // });
   }
 }
+  // deletePost(id:any){
+  //   return this.http.delete(this.url + '/' + id).pipe(
+  //     catchError((error:HttpErrorResponse) => {
+  //       if (error.status === 404) {
+  //         return throwError(new NotFoundError());
+  //       }
+  //       return throwError(new AppError(error));
+  //   })
+  // }
+//}
