b1a34eeb8e47f7ee31df80f303f119683a034320
Sandip Patil <sandip50sandip@gmail.com>
2021-12-01 17:11:53 +0530

    134. Query Parameter

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
 
 @Component({
@@ -9,9 +10,27 @@ import { GithubFollowersService } from '../services/github-followers.service';
 export class GithubFollowersComponent implements OnInit {
   followers:any;
   
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
+
+    this.route.paramMap
+      .subscribe(params => {
+
+      });
+    
+      // let id = this.route.snapshot.paramMap.get('id');
+    
+    this.route.queryParamMap
+      .subscribe(param => {
+
+      });
+      
+    // let pageNumber = this.route.snapshot.queryParamMap.get('page');
+    // console.log(pageNumber);
+
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
